4a762aa1056aa17354b3bb8ab79b4c6219e9826a
emillycaaroline <emillyjchagas@gmail.com>
2024-09-17 11:59:05 -0300

    Criação da pagina login.

---
+++ views/register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar-se</title>
+</head>
+
+<body>
+
+    <div>
+        <h2>Cadastro de Usuário</h2>
+        <form action="" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome" required>
+
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha" required>
+
+            <label for="perfil">Perfil:</label>
+            <select name="perfil" id="perfil">
+                <option value="admin">Admin</option>
+                <option value="gestor">Gestor</option>
+                <option value="colaborador">Colaborador</option>
+            </select>
+
+        </form>
+    </div>
+
+</body>
+
+</html>
