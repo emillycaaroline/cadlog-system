77fcc772126ea11872a87b3c20b9e408875c7cb9
emillycaaroline <emillyjchagas@gmail.com>
2024-09-17 12:50:16 -0300

    Cria condição para metodo post e pega oque foi inserido nos campos de cadstro, userController é controlador de usuário.

---
+++ controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+// Definição da classe UserController, responsável por gerenciar as ações relacionada aos usuários.
+class UserController
+{
+    // Função responsável por cadastrar o usuário.
+    public function register()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = [
+                'nome' => $_POST['nome'], 
+                'email' => $_POST['email'],
+                'senha' => password_hash($_POST['senha'],  PASSWORD_DEFAULT), // = criptografa a senha
+                'perfil' => $_POST['perfil']
+            ];
+
+            User::create($data);
+        } else {
+            // Se a requisição não for POST (por exemplo se for GET), carrega a pagina de resgistro.
+            include 'views/register.php';
+        }
+    }
+}
