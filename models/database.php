053f7e95469c3d6f8e79989874f85252e4cf01bc
emillycaaroline <emillyjchagas@gmail.com>
2024-09-17 12:48:41 -0300

    Cria uma classe database e adiciona comentario sobre ultilizar singlenton.

---
+++ models/database.php
@@ -0,0 +1,5 @@
+<?php
+class Database{
+    // Ultiliza padrão singleton, onde o objetivo é garantir que apenas uma unica instancia de classe seja criada durante a execução do programa e que essa instancia seja ultilizada sempre que necessário
+}
+?>
