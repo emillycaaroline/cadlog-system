853b04ce704a33f358a56dc9cb90d9de32fa3a2a
emillycaaroline <emillyjchagas@gmail.com>
2024-09-17 12:46:20 -0300

    Cria uma condição

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -8,7 +8,10 @@ class  AuthController
     // Cria função responsável pelo processo de login
     public function login()
     {
-        
+        // Verifica se a requisisção HTTP é do tipo POST, ou seja, se o formulário foi enviado
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        }
     }
 }
 ?>
