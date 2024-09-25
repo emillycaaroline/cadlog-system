ee88496511176a1d7af49b380fced723967b5b39
emillycaaroline <emillyjchagas@gmail.com>
2024-09-25 10:14:39 -0300

    Adiciona funções para localizar usuario pelo email ou id e um para conectar no BD

--- models/user.php
+++ models/user.php
@@ -3,8 +3,26 @@ require_once 'models/database.php';
 
 class User
 {
+    // Função para localizar usuário pelo email
     public static function findByEmail($email){
-        
+        $conn = Database::getConnection();    //:: = serve para entrar na classe
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");  // : no sql é um marcador (almenta segurança)
+        $stmt->execute(['email' => $email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    //Função para encontrar usuario pelo ID
+    public static function find($id){
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Função para criar um novo usuário no banco de dados
+    public static function create($data){
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("INSERT INTO usuarios (nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil) ");
+        $stmt->execute($data);
     }
 }
-?>
