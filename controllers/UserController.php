6cc5120c6c11a0b18e4f605795b4f02f982bfb32
emillycaaroline <emillyjchagas@gmail.com>
2024-10-22 12:04:16 -0300

    Adicionei a função de list

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -20,4 +20,9 @@ class UserController
             include 'views/register.php';
         }
     }
+
+    public function list(){
+        $users = User::all();
+        include 'views/list_user.php';
+    }
 }
