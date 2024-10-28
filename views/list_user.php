6e9dd19b6203b62dc19a3bacc8fa69f90af8d885
emillycaaroline <emillyjchagas@gmail.com>
2024-10-28 08:23:10 -0300

    Adiciona link para a pagina de editar

--- views/list_user.php
+++ views/list_user.php
@@ -116,7 +116,7 @@ if (isset($_SESSION['perfil'])) :
                             <td><?= $user['perfil'] ?></td>
                             <td>
                                 <?php if ($_SESSION['perfil'] == 'admin' || $_SESSION['perfil'] == 'gestor') : ?>
-                                    <a href="">Editar</a>
+                                    <a href="index.php?edit&id=<?= $user['id'] ?>">Editar</a>
                                 <?php endif; ?>
 
                                 <?php if ($_SESSION['perfil'] == 'admin') : ?>
