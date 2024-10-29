17911044d4d68625d1ed429b9cdb15a4e8c08539
emillycaaroline <emillyjchagas@gmail.com>
2024-10-29 12:16:30 -0300

    Adiciona css na tela de editar

--- views/edit_user.php
+++ views/edit_user.php
@@ -6,6 +6,115 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Usuário</title>
     <link rel="stylesheet" href="css/edit.css">
+
+    <style>
+           /* corpo da pagina */
+           body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background: rgb(174, 68, 217);
+            background: linear-gradient(0deg, rgba(174, 68, 217, 1) 0%, rgba(233, 177, 255, 1) 100%);
+        }
+
+        /* formulário */
+        div {
+            background: rgb(255, 255, 255);
+            background: radial-gradient(circle, rgba(255, 255, 255, 0.6811099439775911) 0%, rgba(255, 255, 255, 0.773546918767507) 100%);
+            padding: 40px;
+            border-radius: 10px;
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+            width: 500px;
+            max-width: 400px;
+        }
+
+        /* Titulos */
+        label {
+            font-size: large;
+        }
+
+        /* Inputs (Campos para escrever) */
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid black;
+            border-radius: 5px;
+            font-size: medium;
+        }
+
+        /* Campo de selecionar */
+        select {
+
+            padding: 10px;
+            border: 1px solid black;
+            border-radius: 5px;
+            font-size: medium;
+            text-align: center;
+
+
+        }
+
+        /* muda cor qnd clica em select */
+        select:focus {
+            border-color: #be7afd;
+            outline: none;
+            box-shadow: 0 0 5px #be7afd;
+            /* Adiciona um efeito de sombra roxa ao focar */
+
+        }
+
+        /* Muda borda do input ao apertar */
+        input:focus {
+            border-color: #be7afd;
+            outline: none;
+            box-shadow: 0 0 5px #be7afd;
+            /* Adiciona um efeito de sombra roxa ao focar */
+        }
+
+        /* botão */
+        button {
+            width: 100%;
+            padding: 10px;
+            background-color: #be7afd;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            font-size: 1rem;
+            cursor: pointer;
+            transition: background-color 0.3s, box-shadow 0.3s;
+            /* Transição de cor e a sombra */
+        }
+
+        /* Efeito sombra qnd passar o mouse no botão */
+        button:hover {
+            background-color: #a564e9;
+            box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+            /* Sombra mais forte ao passar o mouse */
+        }
+
+        /* link de cadastro */
+        a {
+            display: block;
+            margin-top: 20px;
+            color: #a564e9;
+            transition: color 0.3s;
+            text-align: center;
+        }
+
+        /* Mudando a cor do link qnd passar o mouse */
+        a:hover {
+            color: black;
+        }
+
+        h2 {
+            text-align: center;
+            font-size: x-large;
+            font-family: Georgia, 'Times New Roman', Times, serif;
+        }
+    </style>
+
 </head>
 
 <body class="edit-body">
@@ -18,12 +127,12 @@
             <label for="email">Email:</label>
             <input type="email" name="email" id="email" value="" required>
 
-            <label for="perfil">Perfil:</label>
+            <label for="perfil">Perfil:</label><br>
             <select name="perfil" id="perfil">
                 <option value="">Admin</option>
                 <option value="">Gestor</option>
                 <option value="">Colaborador</option>
-            </select>
+            </select><br><br>
 
             <button type="submit" class="btn">Salvar</button>
         </form>
@@ -33,4 +142,3 @@
 
 </html>
 
-tem menu de contexto
