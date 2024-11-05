37071459ff34a45414d9b1cdf49758be314ffc35
emillycaaroline <emillyjchagas@gmail.com>
2024-11-05 11:41:40 -0300

    Adiciona include para entrar na pagina de edição de usuario

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -42,7 +42,11 @@ class UserController
 
                 User::update($id, $data);
                 header('Location: index.php?action=list');
+            } else{
+                include 'views/edit_user.php';
             }
+        } else{
+            echo 'Você não tem permissão para editar usuários';
         }
     }
 }
